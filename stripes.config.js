3a683ae4ee4ea95ad64200acf33d032d4be4f46f
Mike Taylor <mike@indexdata.com>
2017-05-08 13:29:26 +0100

    Remove the "connect" logging category

--- stripes.config.js
+++ stripes.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
   config: {
-    logCategories: 'core,connect,redux,path,action'
+    logCategories: 'core,redux,path,action'
   },
   modules: {
     '@folio/trivial': {},
