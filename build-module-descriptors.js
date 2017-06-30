fe2343ab05af67de5a8b51da273f4a9c34e40371
Mike Taylor <mike@indexdata.com>
2017-06-30 11:23:24 +0100

    Fix a bunch of ESLint errors

--- build-module-descriptors.js
+++ build-module-descriptors.js
@@ -1,34 +1,34 @@
 const fs = require('fs');
-const child_process = require('child_process');
+const childProcess = require('child_process');
 
 const argv0 = process.argv[1];
 const indir = 'node_modules/@folio';
 const outdir = 'ModuleDescriptors';
 
 console.log('* build-module-descriptors');
-if (!fs.existsSync(outdir)){
+if (!fs.existsSync(outdir)) {
   fs.mkdirSync(outdir);
 }
 
-fs.readdir(indir, function(err, filenames) {
+fs.readdir(indir, (err, filenames) => {
   if (err) {
     console.log(`${argv0}: cannot scan '${err.path}': ${err.message}`);
     process.exit(1);
   }
 
-  filenames = filenames.sort();
-  for (var i = 0; i < filenames.length; i++) {
-    const filename = filenames[i];
+  const sortedFilenames = filenames.sort();
+  for (let i = 0; i < sortedFilenames.length; i++) {
+    const filename = sortedFilenames[i];
     if (filename.startsWith('stripes-')) continue;
     console.log(`processing '${filename}'`);
 
     const cmd = `node ${indir}/stripes-core/util/package2md.js ${indir}/${filename}/package.json > ${outdir}/${filename}.json`;
     try {
-      const buffer = child_process.execSync(cmd);
+      const buffer = childProcess.execSync(cmd);
       const output = buffer.toString();
       if (output) console.log(`'${cmd}' produced unexpected output: '${output}'`);
-    } catch (err) {
-      console.log(`${argv0}: cannot run '${cmd}':`, err);
+    } catch (exception) {
+      console.log(`${argv0}: cannot run '${cmd}':`, exception);
       process.exit(2);
     }
   }
