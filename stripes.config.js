e6806115df37f48b014c4b400d7b7f7efa317649
Wayne Schneider <wayne@indexdata.com>
2017-08-15 15:47:02 -0500

    Remove scan application, add checkout and checkin. Update stripes-core.
    Towards FOLIO-772

--- stripes.config.js
+++ stripes.config.js
@@ -7,7 +7,8 @@ module.exports = {
   modules: {
     '@folio/users': {},
     '@folio/items': {},
-    '@folio/scan': {},
+    '@folio/checkin': {},
+    '@folio/checkout': {},
     '@folio/organization': {},
     '@folio/developer': {},
     '@folio/plugin-markdown-editor': {},
